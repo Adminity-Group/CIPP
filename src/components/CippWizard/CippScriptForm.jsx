96af46aac4470ba1797c366d82ce6affdb0ae17e
ngms-psh <psh@ngms.dk>
2025-03-26 15:36:12 +0100

    Removed duplicate import

--- src/components/CippWizard/CippScriptForm.jsx
+++ src/components/CippWizard/CippScriptForm.jsx
@@ -1,5 +1,4 @@
 import { Grid, Stack } from "@mui/material";
-import { Grid, Stack } from "@mui/material";
 import { CippWizardStepButtons } from "./CippWizardStepButtons";
 import CippJsonView from "../CippFormPages/CippJSONView";
 import CippFormComponent from "../CippComponents/CippFormComponent";
