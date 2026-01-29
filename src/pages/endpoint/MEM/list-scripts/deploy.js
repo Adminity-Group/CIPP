9995287f4dcbf665ed67f1f562403a78c092a089
ngms-psh <psh@ngms.dk>
2026-01-29 21:43:41 +0100

    Updated path for scripts deploy

--- src/pages/endpoint/MEM/list-scripts/deploy.js
+++ src/pages/endpoint/MEM/list-scripts/deploy.js
@@ -1,6 +1,6 @@
-import { Layout as DashboardLayout } from "/src/layouts/index.js";
-import { CippWizardConfirmation } from "/src/components/CippWizard/CippWizardConfirmation";
-import CippWizardPage from "/src/components/CippWizard/CippWizardPage.jsx";
+import { Layout as DashboardLayout } from "../../../../layouts/index.js";
+import { CippWizardConfirmation } from "../../../../components/CippWizard/CippWizardConfirmation";
+import CippWizardPage from "../../../../components/CippWizard/CippWizardPage.jsx";
 import { CippTenantStep } from "../../../../components/CippWizard/CippTenantStep";
 import { CippScriptForm } from "../../../../components/CippWizard/CippScriptForm";
 
